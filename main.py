96d76d0509f08b559fe695cb03f294b79adeffee
Leology0 <leology42@gmail.com>
2023-09-08 12:48:07 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+# Dice Roll Simulator
+import random
+
+def main():
+    randNum = random.randrange(1, 7)
+    # Print Menu
+    print("Dice Roll Simulator Menu")
+    print("1. Roll Dice Once")
+    print("Roll Dice 5 Times")
+    print("Roll Dice 'n' Times")
+    print("Roll Dice until Snake Eyes")
+    print("5. Exit")
+    selection = input("Select An Option (1-5):")
+          
+    
+    
+def roll2Dice(dice1, dice2):
+    print("hello")
+
+
+main()
